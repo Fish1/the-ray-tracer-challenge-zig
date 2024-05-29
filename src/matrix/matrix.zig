b4e2303a4419c31a1a6a63fca0fb7565f8550acd
Jacob Enders <jacobenders1@gmail.com>
2024-05-28 23:49:27 -0400

    update

--- src/matrix/matrix.zig
+++ src/matrix/matrix.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
 pub const Matrix = struct {
     width: usize,
@@ -7,9 +8,12 @@ pub const Matrix = struct {
     buffer: []f64,
     allocator: std.mem.Allocator,
 
-    fn alloc(comptime width: usize, comptime height: usize) !Matrix {
+    pub fn alloc(comptime width: usize, comptime height: usize, data: [width * height]f64) !Matrix {
         const allocator = std.heap.page_allocator;
         const buffer = try allocator.alloc(f64, width * height);
+        for (data, 0..) |number, index| {
+            buffer[index] = number;
+        }
         return Matrix{
             .width = width,
             .height = height,
@@ -20,5 +24,30 @@ pub const Matrix = struct {
 
     pub fn free(self: Matrix) void {
         self.allocator.free(self.buffer);
+    }
+
+    pub fn get(self: Matrix, comptime x: usize, comptime y: usize) !f64 {
+        try expect(x < self.width);
+        try expect(y < self.height);
+        return self.buffer[self.width * y + x];
+    }
+
+    pub fn equals(self: Matrix, other: Matrix) bool {
+        if (self.width != other.width or self.height != other.height) {
+            return false;
+        }
+        const eq = std.math.approxEqAbs;
+        for (0..self.width * self.height) |index| {
+            const a = self.buffer[index];
+            const b = other.buffer[index];
+            if (eq(f64, a, b, 0.00001) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    pub fn multiply(self: Matrix, other: Matrix) !Matrix {
+        try expect(self.width != other.height);
     }
 };
--- src/matrix/test.zig
+++ src/matrix/test.zig
@@ -1,6 +1,77 @@
 const matrix = @import("matrix.zig").Matrix;
+const std = @import("std");
+const expect = std.testing.expect;
 
-test "create matrix" {
-    const m = try matrix.alloc(4, 4);
+test "create matrix 4x4" {
+    const m = try matrix.alloc(4, 4, [16]f64{ 1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5 });
     defer m.free();
+
+    var x = m.get(0, 0) catch 0;
+    try expect(x == 1);
+    x = m.get(3, 0) catch 0;
+    try expect(x == 4);
+    x = m.get(0, 1) catch 0;
+    try expect(x == 5.5);
+    x = m.get(2, 1) catch 0;
+    try expect(x == 7.5);
+    x = m.get(2, 2) catch 0;
+    try expect(x == 11);
+    x = m.get(0, 3) catch 0;
+    try expect(x == 13.5);
+    x = m.get(2, 3) catch 0;
+    try expect(x == 15.5);
+}
+
+test "create matrix 2x2" {
+    const m = try matrix.alloc(2, 2, [4]f64{ -3, 5, 1, -2 });
+    defer m.free();
+
+    var x = m.get(0, 0) catch 0;
+    try expect(x == -3);
+    x = m.get(1, 0) catch 0;
+    try expect(x == 5);
+    x = m.get(0, 1) catch 0;
+    try expect(x == 1);
+    x = m.get(1, 1) catch 0;
+    try expect(x == -2);
+}
+
+test "create matrix 3x3" {
+    const m = try matrix.alloc(3, 3, [9]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 });
+    defer m.free();
+
+    var x = m.get(0, 0) catch 0;
+    try expect(x == -3);
+    x = m.get(1, 0) catch 0;
+    try expect(x == 5);
+    x = m.get(2, 0) catch 0;
+    try expect(x == 0);
+    x = m.get(0, 1) catch 0;
+    try expect(x == 1);
+    x = m.get(1, 1) catch 0;
+    try expect(x == -2);
+    x = m.get(2, 1) catch 0;
+    try expect(x == -7);
+    x = m.get(0, 2) catch 0;
+    try expect(x == 0);
+    x = m.get(1, 2) catch 0;
+    try expect(x == 1);
+    x = m.get(2, 2) catch 0;
+    try expect(x == 1);
+}
+
+test "true equality test" {
+    const m1 = try matrix.alloc(3, 3, [9]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 });
+    defer m1.free();
+    const m2 = try matrix.alloc(3, 3, [9]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 });
+    defer m2.free();
+    try expect(m1.equals(m2) == true);
+}
+
+test "false equality test" {
+    const m1 = try matrix.alloc(3, 3, [9]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 });
+    defer m1.free();
+    const m2 = try matrix.alloc(3, 3, [9]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 2 });
+    defer m2.free();
+    try expect(m1.equals(m2) == false);
 }
