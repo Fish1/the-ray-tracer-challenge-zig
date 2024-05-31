aeafaac83be20d4ace88beb3bb971e79511f58fa
Jacob Enders <jacobenders1@gmail.com>
2024-05-30 21:20:55 -0400

    update

--- src/matrix/matrix.zig
+++ src/matrix/matrix.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const expect = std.testing.expect;
+const ArrayList = std.ArrayList;
 
 pub const Matrix = struct {
     width: usize,
@@ -8,7 +9,7 @@ pub const Matrix = struct {
     buffer: []f64,
     allocator: std.mem.Allocator,
 
-    pub fn alloc(comptime width: usize, comptime height: usize, data: [width * height]f64) !Matrix {
+    pub fn alloc(width: usize, height: usize, data: []const f64) !Matrix {
         const allocator = std.heap.page_allocator;
         const buffer = try allocator.alloc(f64, width * height);
         for (data, 0..) |number, index| {
@@ -26,7 +27,7 @@ pub const Matrix = struct {
         self.allocator.free(self.buffer);
     }
 
-    pub fn get(self: Matrix, comptime x: usize, comptime y: usize) !f64 {
+    pub fn get(self: Matrix, x: usize, y: usize) !f64 {
         try expect(x < self.width);
         try expect(y < self.height);
         return self.buffer[self.width * y + x];
@@ -48,6 +49,30 @@ pub const Matrix = struct {
     }
 
     pub fn multiply(self: Matrix, other: Matrix) !Matrix {
-        try expect(self.width != other.height);
+        try expect(self.width == other.height);
+        const width = other.width;
+        const height = other.height;
+        const allocator = std.heap.page_allocator;
+        var list = ArrayList(f64).init(allocator);
+        for (0..other.height) |row| {
+            for (0..self.width) |col| {
+                const a = try self.get(0, row);
+                const b = try self.get(1, row);
+                const c = try self.get(2, row);
+                const d = try self.get(3, row);
+
+                const e = try self.get(col, 0);
+                const f = try self.get(col, 1);
+                const g = try self.get(col, 2);
+                const h = try self.get(col, 3);
+
+                try list.append(a * e +
+                    b * f +
+                    c * g +
+                    d * h);
+            }
+        }
+        const buffer = try list.toOwnedSlice();
+        return Matrix.alloc(width, height, buffer);
     }
 };
--- src/matrix/test.zig
+++ src/matrix/test.zig
@@ -3,7 +3,10 @@ const std = @import("std");
 const expect = std.testing.expect;
 
 test "create matrix 4x4" {
-    const m = try matrix.alloc(4, 4, [16]f64{ 1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5 });
+    const buffer = [_]f64{ 1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5 };
+    const m = try matrix.alloc(4, 4, &buffer);
+
+    // const m = try matrix.alloc(4, 4, &[_]f64{ 1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5 });
     defer m.free();
 
     var x = m.get(0, 0) catch 0;
@@ -23,7 +26,8 @@ test "create matrix 4x4" {
 }
 
 test "create matrix 2x2" {
-    const m = try matrix.alloc(2, 2, [4]f64{ -3, 5, 1, -2 });
+    const buffer = [_]f64{ -3, 5, 1, -2 };
+    const m = try matrix.alloc(2, 2, &buffer);
     defer m.free();
 
     var x = m.get(0, 0) catch 0;
@@ -37,7 +41,8 @@ test "create matrix 2x2" {
 }
 
 test "create matrix 3x3" {
-    const m = try matrix.alloc(3, 3, [9]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 });
+    const buffer = [_]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 };
+    const m = try matrix.alloc(3, 3, &buffer);
     defer m.free();
 
     var x = m.get(0, 0) catch 0;
@@ -61,17 +66,38 @@ test "create matrix 3x3" {
 }
 
 test "true equality test" {
-    const m1 = try matrix.alloc(3, 3, [9]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 });
+    const buffer1 = [_]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 };
+    const m1 = try matrix.alloc(3, 3, &buffer1);
     defer m1.free();
-    const m2 = try matrix.alloc(3, 3, [9]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 });
+    const buffer2 = [_]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 };
+    const m2 = try matrix.alloc(3, 3, &buffer2);
     defer m2.free();
     try expect(m1.equals(m2) == true);
 }
 
 test "false equality test" {
-    const m1 = try matrix.alloc(3, 3, [9]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 });
+    const buffer1 = [_]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 1 };
+    const m1 = try matrix.alloc(3, 3, &buffer1);
     defer m1.free();
-    const m2 = try matrix.alloc(3, 3, [9]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 2 });
+    const buffer2 = [_]f64{ -3, 5, 0, 1, -2, -7, 0, 1, 2 };
+    const m2 = try matrix.alloc(3, 3, &buffer2);
     defer m2.free();
     try expect(m1.equals(m2) == false);
+}
+
+test "multiply" {
+    const buffer1 = [_]f64{
+        1, 2, 3, 4,
+        5, 6, 7, 8,
+        9, 8, 6, 5,
+        4, 3, 2, 1,
+    };
+    const m1 = try matrix.alloc(4, 4, &buffer1);
+
+    const buffer2 = [_]f64{ -2, 1, 2, 3, 3, 2, 1, -1, 4, 3, 6, 5, 1, 2, 7, 8 };
+    const m2 = try matrix.alloc(4, 4, &buffer2);
+
+    const result = try m1.multiply(m2);
+    const expectedResult = try matrix.alloc(4, 4, &[_]f64{ 20, 22, 50, 48, 44, 54, 114, 108, 40, 58, 110, 102, 16, 26, 46, 42 });
+    try expect(result.equals(expectedResult));
 }
